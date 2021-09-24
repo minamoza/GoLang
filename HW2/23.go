51e4bb1a923f4132c98cf671b9acc4ba11879b40
minamoza <61341637+minamoza@users.noreply.github.com>
2021-09-24 23:15:11 +0600

    Update 23.go

--- HW2/23.go
+++ HW2/23.go
@@ -14,10 +14,18 @@ func (r rot13Reader) Read(b []byte) (int,error) {
 	n, err := r.r.Read(b)
 	
 	for i:= 0; i < len(b); i++{
-		if b[i] >= 'A' && b[i] <= 'Z'{
-			b[i] = byte((b[i]-'A'+13)%26 + 'A')
-		} else if b[i] >= 'a' && b[i] <= 'z'{
-			b[i] = byte((b[i]-'a'+13)%26 + 'a')
+		if b[i] >= 'a' && b[i] <= 'z' {
+			if b[i] >= 'm' {
+				b[i] -= 13
+			} else {
+				b[i] += 13
+			}
+		} else if b[i] >= 'A' && b[i] <= 'Z' {
+			if b[i] >= 'M' {
+				b[i] -= 13
+			} else {
+				b[i] += 13
+			}
 		}
 	}
     return n, err
